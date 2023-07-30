09ed2d8bdfbde9d5b32b5e1e3341442c27b57708
Pavel Potupalo <levapnero@gmail.com>
2023-07-30 19:55:43 +0300

    Hw 1

---
+++ src/main/java/VariablesTheme.java
@@ -0,0 +1,16 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        byte cores = 4;
+        int display = 27;
+        long memory = 16;
+        float freq = 2.7f;
+        double storage = 1.0d;
+        boolean isDesktop = true;
+        short vRAM = 4;
+        char speakers = 0x32 ;
+
+        System.out.print("Cores "+cores+"\nDisplay "+display+"\nMemory "+memory+"\nCore frequency "+freq+"\nStorage "+storage+"\nIs it desktop pc? "+isDesktop+"\nvRAM "+vRAM+"\nSpeakers count "+speakers);
+
+    }
+}
+
