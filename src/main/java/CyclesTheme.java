b191bd69b557c331b691d933bd410bc84352f28c
Pavel Potupalo <levapnero@gmail.com>
2023-08-10 18:14:28 +0300

    hw 3 cycles init

--- src/main/java/CyclesTheme.java
+++ src/main/java/CyclesTheme.java
@@ -1,6 +1,18 @@
 public class CyclesTheme {
     public static void main(String[] args) {
         System.out.println("Task1 Sum count\n");
+        int counter = -10;
+        int even = 0;
+        int noteven = 0;
+        do {
+            int i = counter++;
+            if(i % 2 ==0 ) {
+                even = even + i;
+            } else {
+                noteven = noteven + i;
+            }
 
+        } while (counter>=21);
+        System.out.println("В отрезке [-10, 21] сумма четных чисел = "+even+", а нечетных = "+noteven);
     }
 }
