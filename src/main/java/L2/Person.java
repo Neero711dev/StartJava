a78fd85680eff2ec31756f106d06ecfa2af87e32
Pavel Potupalo <levapnero@gmail.com>
2023-08-11 14:55:48 +0300

    hw lesson 2 init

---
+++ src/main/java/L2/Person.java
@@ -0,0 +1,30 @@
+package L2;
+
+public class Person {
+    String sex = "Male";
+    String name = "Paul";
+    int height = 184;
+    int weight = 71;
+    int age = 30;
+
+    void walk() {
+        System.out.println("you are walking");
+    }
+
+    void sit() {
+        System.out.println("you are sitting");
+    }
+
+    void run() {
+        System.out.println("you are running");
+    }
+
+    void talk() {
+        System.out.println("you are talking");
+    }
+
+    void learn() {
+        System.out.println("you are learning");
+    }
+
+}
